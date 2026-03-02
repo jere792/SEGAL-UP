3f756b23fba955d529529f323a9baf7886d1176e
jere792 <138648408+jere792@users.noreply.github.com>
2026-03-01 23:30:49 -0500

    [01/03/2026] SOLUCIONANDO BUGS 2.2V

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { blogPosts } from './core/data/blog.data/blog.data';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return blogPosts.map(post => ({ slug: post.slug }));
+    }
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
